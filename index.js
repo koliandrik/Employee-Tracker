95792f7e9081064e48e9f082fec690af9d2ab0a4
koliandrik <koliandrik@gmail.com>
2024-07-17 00:46:00 -0700

    added functions to the rest of the options

--- index.js
+++ index.js
@@ -23,6 +23,91 @@ const viewDepartments = async () => {
     startApp();
 }
 
+const viewRoles = async () => {
+    const query = 'SELECT * FROM roles';
+    const { rows } = await pool
+        .query(query);
+    console.table(rows);
+    startApp();
+}
+
+const viewEmployees = async () => {
+    const query = 'SELECT * FROM employees';
+    const { rows } = await pool
+        .query(query);
+    console.table(rows);
+    startApp();
+}
+
+const addDepartment = async () => {
+    const { departmentName } = await inquirer.prompt({
+        name: 'departmentName',
+        type: 'input',
+        message: 'What is the name of the department?'
+    });
+
+    const query = 'INSERT INTO departments (name) VALUES ($1)';
+    await pool.query(query, [departmentName]);
+    console.log('Department added!');
+    startApp();
+}
+
+const addRole = async () => {
+    const { title, salary, departmentId } = await inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of the role?'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary of the role?'
+        },
+        {
+            name: 'departmentId',
+            type: 'input',
+            message: 'What is the department ID of the role?'
+        }
+    ]);
+
+    const query = 'INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)';
+    await pool.query(query, [title, salary, departmentId]);
+    console.log('Role added!');
+    startApp();
+}
+
+const addEmployee = async () => {
+    const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            name: 'roleId',
+            type: 'input',
+            message: 'What is the role ID of the employee?'
+        },
+        {
+            name: 'managerId',
+            type: 'input',
+            message: 'What is the manager ID of the employee?'
+        }
+    ]);
+
+    const query = 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)';
+    await pool.query(query, [firstName, lastName, roleId, managerId]);
+    console.log('Employee added!');
+    startApp();
+}
+
+
 const startApp = async () => {
     const options = [
         'View all departments',
@@ -49,21 +134,27 @@ const startApp = async () => {
             break;
         case 'View all roles':
             // Call a function to view all roles
+            viewRoles();
             break;
         case 'View all employees':
             // Call a function to view all employees
+            viewEmployees();
             break;
         case 'Add a department':
             // Call a function to add a department
+            addDepartment();
             break;
         case 'Add a role':
             // Call a function to add a role
+            addRole();
             break;
         case 'Add an employee':
             // Call a function to add an employee
+            addEmployee();
             break;
         case 'Update an employee role':
             // Call a function to update an employee role
+            updateEmployeeRole();
             break;
         case 'Exit':
             console.log('Goodbye!');
